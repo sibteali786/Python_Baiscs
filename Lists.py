3f222eb5a333a5d9ef631b4b26b511874349b79d
sibteali786 <sibteali786@gmail.com>
2022-03-15 22:54:10 +0500

    added tuple.py and list methods

--- Lists.py
+++ Lists.py
@@ -1,13 +1,39 @@
-def get_word(sentence, n):
-	# Only proceed if n is positive
-	if n > 0:
-		words = sentence.split()
-		# Only proceed if n is not more than the number of words
-		if n <= len(words):
-			return(words[n-1])
-	return("")
+# def get_word(sentence, n):
+# 	# Only proceed if n is positive
+# 	if n > 0:
+# 		words = sentence.split()
+# 		# Only proceed if n is not more than the number of words
+# 		if n <= len(words):
+# 			return(words[n-1])
+# 	return("")
+#
+# print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+# print(get_word("This is a lesson about lists", -4)) # Nothing
+# print(get_word("Now we are cooking!", 1)) # Should print: Now
+# print(get_word("Now we are cooking!", 5)) # Nothing
 
-print(get_word("This is a lesson about lists", 4)) # Should print: lesson
-print(get_word("This is a lesson about lists", -4)) # Nothing
-print(get_word("Now we are cooking!", 1)) # Should print: Now
-print(get_word("Now we are cooking!", 5)) # Nothing
+
+# modifying contents
+fruits = ["pineaple","banana","Apple","Melon"]
+fruits.append("Kiwi")
+print(fruits)
+
+fruits.insert(0,"Orange")   #
+print(fruits)
+
+fruits.insert(25,"Peach")   # even though index 25 does not exist but even then it puts it to last index
+print(fruits)
+
+fruits.remove("Orange") # Removes first occurrence and gives error if not found
+print(fruits)
+
+fruits.pop(5)   # removes element that given index
+print(fruits)
+
+# Iteration
+animals = ["Lion","Zebra","Dolphin","Monkey"]
+chars = 0
+for animal in animals:
+    chars += len(animal)
+
+print("Total characters : {}, Average length: {}".format(chars,chars/len(animals)))
---
+++ tuples.py
@@ -0,0 +1,12 @@
+fullname = ('Grace','M','Hopper')
+# they are immutable and their order never changes
+# we can assign result of a tuple multiple variables
+
+def file_size(file_info):
+	name, type, size= file_info
+	return("{:.2f}".format(size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
