5f1151fd35dce5ed15109c0f097feae793ce1966
sibteali786 <sibteali786@gmail.com>
2022-04-04 00:32:12 +0500

    how to make our own modules

---
+++ Python_OS/ModulesOS.py
@@ -0,0 +1,4 @@
+import areas
+
+print(areas.triangle(5,3))
+print(areas.rectangle(4,5))
---
+++ Python_OS/areas.py
@@ -0,0 +1,14 @@
+import math
+
+
+def triangle(base, height):
+    """calculates area of triangle when base and height are given"""
+    return base * height / 2
+
+
+def rectangle(base, height):
+    return base * height
+
+def circle(radius):
+    return math.pi*(radius**2)
+
