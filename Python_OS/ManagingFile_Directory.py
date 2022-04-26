b849499ad24bf702ba6f9ce8c413fa1a248ea32e
sibteali786 <sibteali786@gmail.com>
2022-04-26 13:43:41 +0500

    os module basics

---
+++ Python_OS/ManagingFile_Directory.py
@@ -0,0 +1,8 @@
+# Code written with this module can easily be run across different Os platforms
+
+# windows, Linux and Mac OS with the precaution the absolute paths must be relevant to that specific OS
+import os
+# os.remove("./hello.txt")    # delete he file with this name \
+
+# os.rename("spider.txt","spiderNew.txt")
+print(os.path.exists("./../CrashCourse/classes.py"))  # true if file exists in path specified
