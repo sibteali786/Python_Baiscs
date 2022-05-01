85491c585c9c4857f9aade3d05874253cf13cb0a
sibteali786 <sibteali786@gmail.com>
2022-05-02 00:59:40 +0500

    added basic regex like "+,*,?,| ,[a-zA-Z]" etc

--- Python_OS/module3_RegularExpression/basicRegularExpressions.py
+++ Python_OS/module3_RegularExpression/basicRegularExpressions.py
@@ -29,3 +29,66 @@ print(check_aei("paramedic")) # True
 
 # for case insensitive
 print(re.search(r"^a","Aelia",re.IGNORECASE)) # matches words starting with a while being case insensitive
+
+# it matches both versions of capitalized and lowercase string
+print(re.search(r"[Pp]ython","Python"))
+print(re.search(r"[Pp]ython","python"))
+
+# we can use a-z to define a range of lowercase characters to match
+print(re.search(r"[a-z]way","the end of the highway"))
+print(re.search(r"[a-z]way","What a way to go"))  # Do not matches as space was not included in a-z
+
+# Exapnding our search results with a-zA-Z0-9
+print(re.search(r"Cloud[a-zA-Z0-9]","Cloudy"))
+print(re.search(r"Cloud[a-zA-Z0-9]","Cloud8"))
+
+# Example
+def check_punctuation (text):
+  result = re.search(r"[,.:;?!]", text)
+  return result != None
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
+
+
+# Carrot sign ^ used in the brackets matches anything except the one mentioned in the brackets
+print(re.search(r"[^a-zA-Z]","This is a Sentence."))  # matches non alphabet letters
+print(re.search(r"[^a-zA-Z ]","This is a Sentence.")) # matche the dot as its only which is not excluded
+
+# The pipe | character matches with either one mentioned in brackets
+print(re.search(r"cat|dog","This is a cat."))
+print(re.search(r"cat|dog","This is a dog."))
+print(re.search(r"cat|dog","I Like cats and dogs."))  # matches only first one
+print(re.findall(r"cat|dog","I like cats and dogs.")) # matches all of them
+
+#  Repeated Matches
+print(re.search(r"Py.*n","Pygmalion"))  # match anything between py and n , zero or n number to times
+print(re.search(r"Py.*n","Python Programming")) # matches all until g as * is greedy in terms of matching
+print(re.search(r"Py[a-z]*n","Python Programming")) # matches the python only as we like to match only characters and
+# not the spaces as well
+print(re.search(r"Py[a-z]*n","Pyn")) # zero match possibility
+
+#  Some other Repetetion qualifiers include plus ( + ) and ( ? )
+# + matches one or more occurrences of the character which comes before it
+print(re.search(r"o+l+","goldfish"))  # matches only the ol
+print(re.search(r"o+l+","woolly"))  # matches ooll as + plus indicates one or more repetition
+print(re.search(r"o+l+","boil"))  # does not match as i comes in between "o" and "l"
+
+# Example
+import re
+def repeating_letter_a(text):
+  result = re.search(r"[a|A].*[a|A]", text)
+  return result != None
+
+print(repeating_letter_a("banana")) # True
+print(repeating_letter_a("pineapple")) # False
+print(repeating_letter_a("Animal Kingdom")) # True
+print(repeating_letter_a("A is for apple")) # True
+
+
+#  ? either zero or one times
+print(re.search(r"p?each","to each their own")) # matches as ? matches zero match
+print(re.search(r"p?each","I like peaches"))  # matches as p occurs only once
